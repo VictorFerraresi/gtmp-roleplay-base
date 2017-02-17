bb670c2dbe42fc8ef607defb407b9efeb4ebc0f7
Luis G. Miki <g.miki@live.com>
2017-02-17 17:40:13 -0200

    Added client script/files transfer to resource and meta.xml automapping on after build event.

---
+++ CreateServerResource.ps1
@@ -0,0 +1,64 @@
+Param (
+    [Parameter(Mandatory=$True)]
+    [string]$SolutionDir
+)
+
+$xml = @"
+<meta>
+    <info name="ProjetoRP Gamemode" author="Manajjdisc" type="script" />
+    <script src="ProjetoRP.dll" type="server" lang="compiled" />
+</meta>
+"@
+
+$ResourceName = "projetorp"
+$SolutionDir = $SolutionDir -replace '"', ""
+$BasePath = "$(Resolve-Path $SolutionDir)\ProjetoRP\Modules"
+
+$ServerResourcePath = "$($SolutionDir)\server\resources\$($ResourceName)"
+$ModulesPath = "$BasePath\*\Client\*"
+$arr = Get-ChildItem $ModulesPath -rec | where { ! $_.PSIsContainer }
+
+$xml = [xml]$xml
+
+foreach ($item in $arr)
+{
+	$RelativePath = $item.fullname -split "Modules\\"
+	$RelativePath = $RelativePath[1]
+	
+	$Folders = $RelativePath -split "\\"
+	$Module = $Folders[0]
+	$File = $Folders[2]
+	
+	Robocopy "$($BasePath)\$($Module)\Client" "$($ServerResourcePath)\$($Module)" /MIR /FFT /Z /XA:H /W:5
+	# New-Item -ItemType Directory -Force -Path "$($ServerResourcePath)\$($Module)"
+	# xcopy $item.fullname "$($ServerResourcePath)\$($Module)" /y
+	
+	if ($item.fullname -like "*.js") 
+	{
+		$Script = $xml.CreateElement("script");
+		
+		#<script src="player_local.js" type="client" lang="javascript" />
+		$ScriptPath = $item.fullname -split [RegEx]::Escape("$($BasePath)")
+		$ScriptPath = $ScriptPath[1] -replace "\\Client", ""
+		
+		$Script.SetAttribute("src", $ScriptPath);
+		$Script.SetAttribute("type", "client");
+		$Script.SetAttribute("lang", "javascript");
+		
+		$xml.meta.AppendChild($Script);
+	} 
+	else 
+	{
+		$Script = $xml.CreateElement("file");
+		
+		#<script src="player_local.js" type="client" lang="javascript" />
+		$ScriptPath = $item.fullname -split [RegEx]::Escape("$($BasePath)")
+		$ScriptPath = $ScriptPath[1] -replace "\\Client", ""
+		
+		$Script.SetAttribute("src", $ScriptPath);
+		
+		$xml.meta.AppendChild($Script);
+	}
+}
+
+$xml.Save("$($ServerResourcePath)\meta.xml");
---
+++ server/resources/projetorp/Player/Player.js
@@ -0,0 +1,49 @@
+﻿var idleCamera = null;
+var idleCameraPos = new Vector3(-183.087, 4803.357, 329.569);
+var idleCameraPlayerPos = new Vector3(-183.087, 4803.357, 329.569 - 15.0);
+var idleCameraLookAt = new Vector3(4.586, 4585.728, 264.119);
+
+API.onResourceStart.connect(function () {
+    API.callNative("_TRANSITION_TO_BLURRED", 2.0);
+    API.setHudVisible(false);
+
+    API.setEntityPositionFrozen(API.getLocalPlayer(), true);
+    API.setEntityPosition(API.getLocalPlayer(), idleCameraPlayerPos);
+
+    idleCamera = API.createCamera(idleCameraPos, new Vector3(0, 0, 0));
+    API.pointCameraAtPosition(idleCamera, idleCameraLookAt);
+    API.setActiveCamera(idleCamera);
+
+    API.triggerServerEvent("CS_PLAYER_PRELOAD_READY");
+});
+
+var charselCamera = null;
+var charselPlayerPos = new Vector3(-1150.293, -1513.864, 9.633);
+var charselCameraLookAt = new Vector3(-1150.293, -1513.864, 10.633);
+var charselCameraPos = new Vector3(-1146.890, -1512.382, 10.633);
+var charselPlayerRot = new Vector3(0, 0, 299.432);
+
+API.onServerEventTrigger.connect(function (eventName, args) {
+    if (eventName == "SC_DO_CHARSEL") {
+        if (idleCamera != null) {
+            // Is it enough?
+            idleCamera = null;
+        }
+        API.callNative("_TRANSITION_FROM_BLURRED", 2.0);
+
+        var player = API.getLocalPlayer();
+        var charselCamera = API.createCamera(charselCameraPos, new Vector3(0, 0, 0));
+        API.pointCameraAtPosition(charselCamera, charselCameraLookAt);
+
+        // API.callNative("SET_PLAYER_CONTROL", API.getLocalPlayer(), false, 0);
+
+        // API.setPlayerSkin(-257153498);
+        API.setEntityPosition(API.getLocalPlayer(), charselPlayerPos);
+        API.setEntityRotation(API.getLocalPlayer(), charselPlayerRot);
+
+        API.setActiveCamera(charselCamera);
+    } else if (eventName == "SC_DO_SPAWN") {
+        API.setActiveCamera(null);
+        API.setHudVisible(true);
+    }
+});
